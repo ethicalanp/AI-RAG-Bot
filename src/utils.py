417283e99b9e707a937843d924b07de0b22f4abb
ethicalanp <ethicalanp66@gmail.com>
2026-01-14 19:42:30 +0530

    sourse utils file

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def split_text(text, chunk_size=1000, chunk_overlap=100):
+    """
+    Splits a large text into smaller chunks suitable for embedding.
+    """
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+    return splitter.split_text(text)
